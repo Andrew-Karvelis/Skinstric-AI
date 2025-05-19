a4c9c6bfce1eef4b279436415f29164c64fd7416
Andrew-Karvelis <akarvelis.work@gmail.com>
2025-05-19 23:15:52 +1000

    Fixed errors for build deployment

--- src/app/analysis/page.tsx
+++ src/app/analysis/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useImageStore } from "@/store/imageStore";
 
 function Analysis() {
